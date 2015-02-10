19b9aee18a41cfbd36724e5901312c9898d80a66
Ryan Jones <ryan.jones26@gmail.com>
2015-02-10 09:38:23 -0500

    not sure

--- Gemfile
+++ Gemfile
@@ -2,10 +2,9 @@
 source "https://rubygems.org"
 
 # gem "rails"
-
-group :test  do
-gem "test-kitchen"
-gem "kitchen-ansible"
-gem "serverspec"
-gem "kitchen-vagrant"
+group :test :integration do
+  gem "test-kitchen"
+  gem "serverspec"
+  gem "kitchen-ansible"
+  gem "kitchen-digitalocean"
 end
---
+++ test/integration/default/serverspec/nginx_spec.rb
@@ -0,0 +1,17 @@
+require 'serverspec'
+include Serverspec::Helper::Exec
+include Serverspec::Helper::DetectOS
+
+describe " nginx::default" do
+  it " installed nginx" do
+    expect(package('nginx')).to be_installed
+  end
+  it " should run nginx" do
+    expect(service('nginx')).to be_running
+  end
+
+  it " should have enabled nginx on boot" do
+    expect(service('nginx')).to be_enabled
+  end
+
+end
